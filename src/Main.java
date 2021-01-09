acce9ac1b3a8a18f9331f1d07a9ff9c6fb214fc5
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-01-09 20:49:40 +0530

    Testing the logic for finding the root element

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        String string = "MERRYCHRISTMAS";
-
-        // Depth 4
-        Node node1 = new Node(string.charAt(0), null, null);
-        Node node3 = new Node(string.charAt(2), null, null);
-        Node node5 = new Node(string.charAt(4), null, null);
-        Node node7 = new Node(string.charAt(6), null, null);
-        Node node9 = new Node(string.charAt(8), null, null);
-        Node node11 = new Node(string.charAt(10), null, null);
-        Node node13 = new Node(string.charAt(12), null, null);
-
-        // Depth 3
-        Node node2 = new Node(string.charAt(1), node1, node3);
-        Node node6 = new Node(string.charAt(5), node5, node7);
-        Node node10 = new Node(string.charAt(9), node9, node11);
-        Node node14 = new Node(string.charAt(13), node13, null);
-
-        // Depth 2
-        Node node4 = new Node(string.charAt(3), node2, node6);
-        Node node12 = new Node(string.charAt(11), node10, node14);
-
-        // Depth 1
-        Node root = new Node(string.charAt(7), node4, node12);
-
-        Graph graph = new Graph();
-        graph.inOrder(root);
+        Utils utils = new Utils();
+        String string = "ABCDEFGHIJLKMN";
+        int length = utils.getClosesPossibleCompleteBinaryTreeSize(string.length());
+        String string1 = string.substring(0, length);
+        char middle = string.charAt(length);
+        String string2 = string.substring(length + 1);
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(middle);
 
         /*
 
