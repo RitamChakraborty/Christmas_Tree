8268a584598df0eb06396677bd54ceaf0bc8bcfb
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-02-08 10:39:49 +0530

    Add command line entry

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import controller.TreeGeneratorController;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final TreeGeneratorController treeGeneratorController = new TreeGeneratorController();
+
     public static void main(String[] args) {
-        String string = "ABCD";
-        TreeGeneratorController treeGeneratorController = new TreeGeneratorController();
-        System.out.println(treeGeneratorController.getDiagram(string));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("********** Christmas Tree **********");
+        System.out.println("Enter a text: ");
+        String string = scanner.nextLine();
+        String diagram = treeGeneratorController.getDiagram(string);
+
+        System.out.println(diagram);
     }
 }
